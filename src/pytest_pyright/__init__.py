2c912c822fe8ecffe299a06025414a7c99c25a9a
Robert Craigie <robert@craigie.dev>
2022-11-20 13:52:34 +0000

    feat(package): release v0.0.3

--- src/pytest_pyright/__init__.py
+++ src/pytest_pyright/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'pytest-pyright'
 __author__ = 'RobertCraigie'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 RobertCraigie'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 from . import plugin
